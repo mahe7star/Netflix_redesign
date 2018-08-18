bb42088c863e042cd3aa87dd3daa7c279737b0a5
mahe7star <mp1995singh@gmail.com>
2018-08-18 22:05:44 +0530

    trending section

--- main.js
+++ main.js
@@ -5,4 +5,35 @@ $(function() {
         autoplay: true,
         autoplaySpeed: 8000
     });
+    $('.category-carousel').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        infinite: false,
+    });
+    $('.trending-carousel').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 6,
+        arrows:true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+    })
 });
