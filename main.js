574a0d8a8f16a75c4839da51b4bbbc975112028f
mahe7star <mp1995singh@gmail.com>
2018-08-19 12:17:33 +0530

    rating range added

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 $(function() {
+    $(window).on('scroll', function(event) {
+      var scrollValue = $(window).scrollTop();
+      if ( scrollValue > 70) {
+          $('.navbar').addClass('fixed-top');
+      } 
+      if(scrollValue < 70) {
+        $('.navbar').removeClass('fixed-top');
+      }
+    });
+
     $('.slick-car').slick({
         dots: true,
         arrows: false,
@@ -42,4 +52,20 @@ $(function() {
         slidesToScroll: 1,
         infinite: false,
     });
+
+    // range slider
+    $('input[type="range"]').change(function () {
+      
+      var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+      console.log($(this)[0].value);
+      $('#ratingValue').text($(this)[0].value);
+
+      $(this).css('background-image',
+                  '-webkit-gradient(linear, left top, right top, '
+                  + 'color-stop(' + val + ', #ffc107), '
+                  + 'color-stop(' + val + ', #C5C5C5)'
+                  + ')'
+                  );
+  });
+    
 });
